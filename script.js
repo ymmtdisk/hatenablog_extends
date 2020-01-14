b3a3569c35c95b4d5fd5f86a83beac0907216216
Daisuke Yamamoto <rhythm.and.days@gmail.com>
2020-01-14 16:22:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,8 @@ jQuery(function($) {
   ).each(function(i, obj) {
     var $obj = $(obj);
 //     if (!$obj.is("li") && !$obj.isFullOfElements()) return; // 内容が要素のみでなければ次へ
-    if (!$obj.isFullOfElements()) return; // 内容が要素のみでなければ次へ
+//     if (!$obj.isFullOfElements()) return; // 内容が要素のみでなければ次へ
+    if ($obj.empty().text()!="") return; // 内容が要素のみでなければ次へ
     if (!$obj.find("a").length) return; // aタグがなければ次へ
     var $item = $($obj.find("a").get(0));
     if ($item.attr("href").indexOf("#") == 0) return; // アンカーであれば次へ
