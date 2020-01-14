3b711b7f1a51b754b769034650e522e6f0dbb011
Daisuke Yamamoto <rhythm.and.days@gmail.com>
2020-01-14 19:17:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ jQuery(function($) {
     this.children().each(function(j, obj) {
       childrensText += $(obj).text();
     });
-    console.log(this.get(0).innerHtml);
+    console.log(this.html());
     return innerText == childrensText;
   };
 
