de9090494b5182ec6aecdbe067164074591b8a40
Daisuke Yamamoto <3234360+ymmtdisk@users.noreply.github.com>
2021-04-12 17:37:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ jQuery(function($) {
   $(".entry-content a").each(function(i, obj) {
     var $item = $(obj);
     if ($item.prop("href") && $item.prop("href").indexOf("#") == 0) return; // アンカーであれば次へ
-    $item.after(hateb($item.attr("href")));
+    $item.after(hateb($item.prop("href")));
   });
 
   /**
