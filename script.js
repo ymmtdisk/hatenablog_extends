decbcc77e6825d616a385bc19a3f6c57f8ec1948
Daisuke Yamamoto <3234360+ymmtdisk@users.noreply.github.com>
2023-07-18 11:58:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ jQuery(function($) {
    * タグを除外して検索・置換するように修正
    * アトリビュート以外を確認して置換するように修正
    */
-  $(".entry-content p, .entry-content li").each(function(){
+  $(".entry-content :header, .entry-content p, .entry-content li").each(function(){
     var text = $(this).html();
     var _text = $(this).html();
     var tags = [];
